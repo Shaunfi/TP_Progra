a8ad73dce5a743079a02c24c97cc25e52c98f936
Jose Giraudo <josegiraudo7@gmail.com>
2023-11-07 17:50:56 -0300

    empezando con los controllers

---
+++ Proyecto/APIFarmacia/Controllers/ClientesController.cs
@@ -0,0 +1,53 @@
+﻿using Backend.Entidades;
+using Microsoft.AspNetCore.Mvc;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace APIFarmacia.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ClientesController : ControllerBase
+    {
+        private static List<Clientes> lClientes = new List<Clientes>();
+
+        // GET: api/<ClientesController>
+        [HttpGet]
+        public IActionResult Get()
+        {
+
+            return Ok(lClientes);
+        }
+
+        //// GET api/<ClientesController>/5
+        //[HttpGet("{id}")]
+        //public string Get(int id)
+        //{
+        //    return "value";
+        //}
+
+        // POST api/<ClientesController>
+        [HttpPost]
+        public IActionResult Post(Clientes dto)
+        {
+            if(dto != null)
+            {
+                return BadRequest();
+            }
+            lClientes.Add(dto);
+            return Ok("Se registro el cliente exitosamente!");
+        }
+
+        //// PUT api/<ClientesController>/5
+        //[HttpPut("{id}")]
+        //public void Put(int id, [FromBody] string value)
+        //{
+        //}
+
+        //// DELETE api/<ClientesController>/5
+        //[HttpDelete("{id}")]
+        //public void Delete(int id)
+        //{
+        //}
+    }
+}
---
+++ Proyecto/APIFarmacia/Controllers/ProductosController.cs
@@ -0,0 +1,54 @@
+﻿using Backend.Entidades;
+using Microsoft.AspNetCore.Mvc;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace APIFarmacia.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ProductosController : ControllerBase
+    {
+
+        private static List<Productos> lProductos = new List<Productos>();
+
+        // GET: api/<ProductosController>
+        [HttpGet]
+        public IActionResult Get()
+        {
+
+            return Ok(lProductos);
+        }
+
+        //// GET api/<ProductosController>/5
+        //[HttpGet("{id}")]
+        //public string Get(int id)
+        //{
+        //    return "value";
+        //}
+
+        // POST api/<ProductosController>
+        [HttpPost]
+        public IActionResult Post(Productos dto)
+        {
+            if (dto != null)
+            {
+                return BadRequest();
+            }
+            lProductos.Add(dto);
+            return Ok("Se registro el producto exitosamente!");
+        }
+
+        //// PUT api/<ProductosController>/5
+        //[HttpPut("{id}")]
+        //public void Put(int id, [FromBody] string value)
+        //{
+        //}
+
+        //// DELETE api/<ProductosController>/5
+        //[HttpDelete("{id}")]
+        //public void Delete(int id)
+        //{
+        //}
+    }
+}
